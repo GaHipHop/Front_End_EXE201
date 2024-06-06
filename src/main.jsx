49434cb2d4edcee45723891da59f59e895bbcc62
Phạm Quốc Đạt <134580007+datpq-dev@users.noreply.github.com>
2024-06-07 02:08:30 +0700

    add Product page, ProductDetail page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Login from "./components/pages/Login";
+import Product from "./components/pages/Product";
+import ProductDetail from "./components/pages/ProductDetail";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,6 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productDetail" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
